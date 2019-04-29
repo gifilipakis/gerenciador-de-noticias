f233c4dac32e800deb4f805881e8901d4acd6512
gifilipakis <giovannafilipakis@Giovannas-MacBook-Pro.local>
2019-04-29 12:23:57 -0300

    Falta só o ranking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GerenciadorService } from './gerenciador.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gerenciador-noticias';
+  tela = 'inicial';
+  autor = null;
+  titulo = null;
+  conteudo = null;
+  views = 0;
+  visualizar = null;
+
+  constructor(private service: GerenciadorService) {}
+
+  telaInicial(){
+    this.tela = 'inicial';
+  }
+
+  telaLer(){
+    this.tela = 'ler';
+  }
+
+  telaCadastro(){
+    this.tela = 'cadastro';
+  }
+
+  telaSalvas(){
+    this.tela = 'salvas';
+  }
+
+  telaEstatisticas(){
+    this.tela = 'estatisticas';
+  }
+
+  salvar(){
+    this.tela = 'salvas';
+    this.service.salvarNoticia(this.autor, this.titulo, this.conteudo, this.views);
+    this.autor = null;
+    this.titulo = null;
+    this.conteudo = null;
+  }
+
+  ler(publicada){
+    publicada.views++;
+    this.tela = 'ler'
+    this.visualizar = publicada
+  }
+
 }
+
+
+  
---
+++ src/app/gerenciador.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GerenciadorService {
+  noticias = [];
+  publicadas = [];
+  autores = ['Fabiano Fagundes', 'Cristina Filipakis', 'Jackson Gomes'];
+
+  constructor() { }
+  
+  salvarNoticia(autor, titulo, conteudo, views){
+    const noticia = {
+      id: this.noticias.length,
+      titulo: titulo,
+      autor: autor,
+      conteudo: conteudo,
+      views: views,
+    }
+    this.noticias.push(noticia);
+  }
+
+  publicar(noticia){
+    this.publicadas.push(noticia);
+  }
+
+  noticiasPorAutor(a){
+    var qnt = 0
+    for(let n of this.noticias){
+      if (n.autor === a){
+        qnt++
+      }
+    }
+    if(!qnt){
+      return 0
+    }
+    return qnt
+  }
+
+  autorMaisPublica(){
+    var maior_qnt = 0
+    var autor = null
+    for(let a of this.autores){
+      var qnt = 0
+      for(let p of this.publicadas){
+        if (p.autor === a){
+          qnt++
+        }
+      }
+      if(qnt > maior_qnt){
+        maior_qnt = qnt
+        autor = a
+      }
+    }
+    return autor
+  }
+  
+}
